79060ef009efed2347fc9a6529f0660bff35b866
mdcoon <mdcoon1@yahoo.com>
2024-06-04 15:47:52 -0400

    Added IAssetHook to commit

---
+++ contracts/asset/IAssetHook.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.24;
+
+/**
+ * Some assets may require customization when minting and transferring assets. This interface
+ * allows an issuing company to extend the default behavior for minting and transfers.
+ */
+interface IAssetHook {
+
+    /**
+     * Called before minting assets to the given 'to' address. 
+     * @return false means the minting process should NOT proceed.
+     */
+    function beforeMint(address asset, address to, uint256 amtOrTokenId) external returns (bool);
+
+    /**
+     * Called before revoking tokens from a target address.
+     * @return false means revoking should NOT proceed 
+     */
+    function beforeRevoke(address asset, address target, uint256 amtOrTokenId) external returns (bool);
+
+    /**
+     * Called before a transfer occurs. 
+     * @return false means the transfer should NOT proceed.
+     */
+    function beforeTransfer(address asset, address to, uint256 amtOrTokenId) external returns (bool);
+}
